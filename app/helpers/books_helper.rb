3b65277d19ea33aac7aac352cf8ba197c26e7df0
preethika priya <preethikapriya9@gmail.com>
2017-04-21 00:55:08 +0530

    updated books, book_user

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,5 @@
 module BooksHelper
+	def book_borrowed(current_user,book_id)
+		borrowed = current_user.book_users.where('book_id=?',book_id).empty?
+	end
 end
--- app/models/book_user.rb
+++ app/models/book_user.rb
@@ -2,14 +2,16 @@ class BookUser < ApplicationRecord
 	belongs_to :user 
 	belongs_to :book
 	belongs_to :status
+	after_save :update_book_availability_count
 
 	def self.create_book_user(id,current_user,book_user)
 		book_user.book_id = Book.where('id=?',id).first.id
-    book_user.user_id = current_user.id
-    book_user.from = Date.today
-    book_user.to = Date.today + 15.days
-    book_user.status_id = Status.all.first.id
-    book_user.save
+	    book_user.user_id = current_user.id
+	    book_user.from = Date.today
+	    book_user.to = Date.today + 15.days
+	    book_user.status_id = Status.all.first.id
+	    binding.pry
+	    book_user.save
 	end
 
 	def self.set_current_user_records(current_user)
@@ -19,5 +21,15 @@ class BookUser < ApplicationRecord
 			@book_users = current_user.book_users
 		end	    
 	end
+
+	def update_book_availability_count
+	book = Book.where('id=?',book_id).first
+		if status_id == Status.where('name=?',"Borrowed").first.id
+			book.borrowed_count = book.borrowed_count.to_i + 1
+		elsif status_id == Status.where('name=?',"Returned").first.id
+			book.borrowed_count  = book.borrowed_count.to_i - 1
+		end
+		book.save
+	end	
 
 end
