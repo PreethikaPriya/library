3da4180b66d106887bf3c048a66531ca7c64c7a5
preethika priya <preethikapriya9@gmail.com>
2017-04-22 00:19:42 +0530

    updated books, book users

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -2,4 +2,11 @@ module BooksHelper
 	def book_borrowed(current_user,book_id)
 		borrowed = current_user.book_users.where('book_id=?',book_id).empty?
 	end
+
+	def book_borrowed_status(user_id,book_id)
+		status = Status.where('name!=?',"Returned").pluck(:id)
+		current_userbooks = BookUser.where('book_id=? and user_id=?', book_id, user_id)
+		current_userbook_status = current_userbooks.where(status_id: status)
+		current_userbook_status.empty?
+	end
 end
--- app/models/book_user.rb
+++ app/models/book_user.rb
@@ -4,13 +4,22 @@ class BookUser < ApplicationRecord
 	belongs_to :status
 	after_save :update_book_availability_count
 
+	#validate :check_currnet_book_status
+
+	def check_currnet_book_status
+		status_id = Status.where('name=?',"Returned").first.id
+		current_userbook_status = BookUser.where('book_id=? and user_id=? and status_id!=?', book_id, user_id, status_id)
+		if !current_userbook_status.empty?
+			errors.add(:book_id, "You have already borrowed this book and havent returend yet!")
+		end	
+	end
+
 	def self.create_book_user(id,current_user,book_user)
 		book_user.book_id = Book.where('id=?',id).first.id
 	    book_user.user_id = current_user.id
 	    book_user.from = Date.today
 	    book_user.to = Date.today + 15.days
 	    book_user.status_id = Status.all.first.id
-	    binding.pry
 	    book_user.save
 	end
 
@@ -23,7 +32,7 @@ class BookUser < ApplicationRecord
 	end
 
 	def update_book_availability_count
-	book = Book.where('id=?',book_id).first
+		book = Book.where('id=?',book_id).first
 		if status_id == Status.where('name=?',"Borrowed").first.id
 			book.borrowed_count = book.borrowed_count.to_i + 1
 		elsif status_id == Status.where('name=?',"Returned").first.id
