0c3d0177e9d620dea5c8a3adac6d017ee28603f5
preethika priya <preethikapriya9@gmail.com>
2017-05-01 18:55:13 +0530

    updated dependant destroy correctly

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,8 @@
 class Book < ApplicationRecord
 	belongs_to :category
 
-	has_many :book_users,dependent: :destroy
-	has_many :users ,through: :book_users
+	has_many :book_users
+	has_many :users ,through: :book_users, dependent: :destroy
 
 	validates_presence_of :name ,:author ,:category_id, :availability
 end
